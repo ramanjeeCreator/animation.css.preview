e476b476cbdc6621d91ca6891802e8c0b8917f1b
Raman Jee <ramanjee0806@gmail.com>
2023-08-16 20:48:04 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+console.log('Raman is now live');
+
+let changeIt = document.querySelector('h1');
+
+let btn = document.querySelector('button');
+btn.addEventListener('click', () => {
+    document.querySelector('button').style.transform = 'translate(1vw, 1vw)';
+    setTimeout(() => {
+        document.querySelector('button').style.transform = 'none';
+    }, 100);
+
+    let addClass = document.querySelector('select').value;
+    console.log(addClass);
+    changeIt.removeAttribute('class');
+    changeIt.setAttribute('class', `animate__animated`);
+    setTimeout(()=>{
+        changeIt.classList.add(addClass);
+    },100);
+});
